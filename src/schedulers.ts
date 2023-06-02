36f2b167e38f77a5c0f8817f7c3da74fad62373d
httpassoca <me@passoca.dev>
2023-06-02 23:50:40 +0100

    add scheduler friday fast

--- src/schedulers.ts
+++ src/schedulers.ts
@@ -1,8 +1,9 @@
 import { Bot } from "grammy";
-
 var cron = require('node-cron');
 
 const PUCUNAID = -1001150475405;
+
+const videoSextaLink = 'https://tbnaluslgxzikblascgb.supabase.co/storage/v1/object/sign/passoca/videos/sextafeirameiodia.mp4?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJwYXNzb2NhL3ZpZGVvcy9zZXh0YWZlaXJhbWVpb2RpYS5tcDQiLCJpYXQiOjE2ODU3NDYxMzksImV4cCI6MTcxNzI4MjEzOX0.M80dt3M6XUW0CUnkT_v6cwqo6LcrxVoUEbpHrV9DoJc&t=2023-06-02T22%3A49%3A00.644Z'
 
 export const schedulers = (bot: Bot) => {
   // Saturday 4pm
@@ -10,4 +11,12 @@ export const schedulers = (bot: Bot) => {
     console.log('Sent roleta scheduled');
     bot.api.sendMessage(PUCUNAID, 'Tem roleta amanhã 22h')
   });
+
+  // Friday 12am
+  cron.schedule('0 12 * * 5', () => {
+    console.log('Sent friday fast scheduled');
+    bot.api.sendVideo(PUCUNAID, videoSextaLink)
+  });
+
+
 }
