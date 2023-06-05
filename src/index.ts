49b961d3b66655273a4d25bc16487f63ceb6a53d
httpassoca <me@passoca.dev>
2023-06-05 01:31:00 +0100

    fix triggers

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ bot.on("message:text", async (ctx) => {
     await ctx.reply('de cima é viado KKKKKKKKKKKKKK');
   }
 
-  if (ctx.message.text.includes('gpt')) {
+  if (ctx.message.text.includes('gpt ')) {
     const answer = await gptAnswer(ctx.message.text, ctx.message.forward_sender_name);
     await ctx.reply(answer, { reply_to_message_id: ctx.message.message_id });
   }
@@ -25,7 +25,7 @@ bot.on("message:text", async (ctx) => {
     await ctx.replyWithPhoto(imgUrl, { reply_to_message_id: ctx.message.message_id });
   }
 
-  if (ctx.message.text.includes('deus')) {
+  if (ctx.message.text.includes(' deus ')) {
     const img = 'https://i.imgur.com/nfZV54N.jpg';
     await ctx.replyWithPhoto(img, { reply_to_message_id: ctx.message.message_id })
   }
