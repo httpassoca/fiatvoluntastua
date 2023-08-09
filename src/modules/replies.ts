a67543e42a346c63ba47982f454b546c0dd8fc59
Rafael Freitas <49541181+httpassoca@users.noreply.github.com>
2023-08-09 16:18:27 +0100

    remove gptimg

--- src/modules/replies.ts
+++ src/modules/replies.ts
@@ -1,6 +1,6 @@
 import { Bot } from "grammy";
 import { gptAnswer } from "../services/gptAnswer";
-import { gptAnswerImage } from "../services/gptAnswerImage";
+// import { gptAnswerImage } from "../services/gptAnswerImage";
 
 let alvaroViado = true;
 
@@ -13,7 +13,7 @@ export const addReplies = (bot: Bot) => {
     }
 
     if (ctx.message.text.includes('gpt ')) {
-      const answer = await gptAnswer(ctx.message.text, ctx.message.forward_sender_name);
+      const answer = await gptAnswer(ctx.message.text.replace('gpt ', ''), ctx.message.forward_sender_name);
       if (answer.length > 4000) {
         let half = answer.slice(0, answer.length / 2);
         await ctx.reply(half, replyMessage);
@@ -24,14 +24,14 @@ export const addReplies = (bot: Bot) => {
       }
     }
 
-    if (ctx.message.text.includes('gptimg')) {
-      try {
-        const imgUrl = await gptAnswerImage(ctx.message.text);
-        await ctx.replyWithPhoto(imgUrl, replyMessage);
-      } catch (error) {
-        await ctx.reply(error as string, replyMessage);
-      }
-    }
+    // if (ctx.message.text.includes('gptimg')) {
+    //   try {
+    //     const imgUrl = await gptAnswerImage(ctx.message.text);
+    //     await ctx.replyWithPhoto(imgUrl, replyMessage);
+    //   } catch (error) {
+    //     await ctx.reply(error as string, replyMessage);
+    //   }
+    // }
 
     if (/\bdeus\b/.test(ctx.message.text)) {
       const img = 'https://i.imgur.com/nfZV54N.jpg';
